a287e751a32d58117a4bdb6b77ac385ababa432e
nbur4556 <nbur4556@gmail.com>
2020-11-21 16:55:15 -0600

    creates initial stylesheet for navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 // Components
 import Toolbar from './components/Toolbar.js';
 
+// Stylesheets
+import './stylesheets/reset.css';
+
 function App() {
     return (
         <Toolbar />
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../stylesheets/toolbar.css';
 
 const Toolbar = () => {
     return (
