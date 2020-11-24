cdae3104961912602caab4e1879e493c98daaf2b
nbur4556 <nbur4556@gmail.com>
2020-11-24 16:08:15 -0600

    uses dropdown options input to select filter options instead of multiple input boxes

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -36,32 +36,20 @@ class Toolbar extends React.Component {
                 {/* Toolbar Options */}
                 <ToolbarOptions key="1" barState={this.state.filterOptionsBar}>
                     <form>
-                        <label name="">ID</label>
-                        <input name="filter-id" type="text" />
+                        <label for="filter-by">Filter By</label>
+                        <select name="filter-by">
+                            <option value="id">ID</option>
+                            <option value="first-name">First Name</option>
+                            <option value="last-name">Last Name</option>
+                            <option value="position">Position</option>
+                            <option value="salary">Salary</option>
+                            <option value="phone-number">Phone Number</option>
+                            <option value="email">Email</option>
+                            <option value="address">Address</option>
+                            <option value="hire-date">Hire Date</option>
+                        </select>
 
-                        <label name="">First Name</label>
-                        <input name="filter-firstName" type="text" />
-
-                        <label name="">Last Name</label>
-                        <input name="filter-lastName" type="text" />
-
-                        <label name="">Position</label>
-                        <input name="filter-position" type="text" />
-
-                        <label name="">Salary</label>
-                        <input name="filter-salary" type="text" />
-
-                        <label name="">Phone Number</label>
-                        <input name="filter-phoneNumber" type="text" />
-
-                        <label name="">Email</label>
-                        <input name="filter-email" type="text" />
-
-                        <label name="">Address</label>
-                        <input name="filter-address" type="text" />
-
-                        <label name="">Hire Date</label>
-                        <input name="filter-hireDate" type="text" />
+                        <input type='text' placeholder="Value" />
 
                         <button type="submit">Filter</button>
                     </form>
