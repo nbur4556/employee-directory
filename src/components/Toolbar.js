0a8913cf639de3a29ad02698af9a850c88d1bcfb
nbur4556 <nbur4556@gmail.com>
2020-11-24 14:38:16 -0600

    toolbar options uses labels opposed to placeholders

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -36,15 +36,24 @@ class Toolbar extends React.Component {
                 {/* Toolbar Options */}
                 <ToolbarOptions key="1" barState={this.state.filterOptionsBar}>
                     <form>
-                        <input name="filter-id" type="text" placeholder="Text" />
-                        <input name="filter-firstName" type="text" placeholder="Text" />
-                        <input name="filter-lastName" type="text" placeholder="Text" />
-                        <input name="filter-position" type="text" placeholder="Text" />
-                        <input name="filter-salary" type="text" placeholder="Text" />
-                        <input name="filter-phoneNumber" type="text" placeholder="Text" />
-                        <input name="filter-email" type="text" placeholder="Text" />
-                        <input name="filter-address" type="text" placeholder="Text" />
-                        <input name="filter-hireDate" type="text" placeholder="Text" />
+                        <label name="">ID</label>
+                        <input name="filter-id" type="text" />
+                        <label name="">First Name</label>
+                        <input name="filter-firstName" type="text" />
+                        <label name="">Last Name</label>
+                        <input name="filter-lastName" type="text" />
+                        <label name="">Position</label>
+                        <input name="filter-position" type="text" />
+                        <label name="">Salary</label>
+                        <input name="filter-salary" type="text" />
+                        <label name="">Phone Number</label>
+                        <input name="filter-phoneNumber" type="text" />
+                        <label name="">Email</label>
+                        <input name="filter-email" type="text" />
+                        <label name="">Address</label>
+                        <input name="filter-address" type="text" />
+                        <label name="">Hire Date</label>
+                        <input name="filter-hireDate" type="text" />
                         <button type="submit">Filter</button>
                     </form>
                 </ToolbarOptions>
